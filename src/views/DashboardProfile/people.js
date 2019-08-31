250ea2a385ab55fcb01de589d59df0d3584f108a
Luc Somers <lucwsomers@gmail.com>
2019-08-31 11:13:22 -0300

    add build reqs

--- src/views/DashboardProfile/people.js
+++ src/views/DashboardProfile/people.js
@@ -43,6 +43,74 @@ const people = {
       expressive: "35%"
     },
     topics: ["Vegano", "Java", "Pop Vegan", "Loving Hut", "Supreme Leader"]
+  },
+  luc: {
+    profilePicture:
+      "https://media.licdn.com/dms/image/C4D03AQGiS1hmpr0XaA/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=DJRYOHS_9WP3RS7Ep8yzeRyblhwyumzMt6vf92reUTo",
+    name: "Luc Somers",
+    email: "lucwsomers@quintoandar.com",
+    company: "QuintoAndar",
+    position: "Software Engineer",
+    city: "São Paulo",
+    country: "Brasil",
+    quote:
+      "I am a result driven Software Engineer currently working with Java, Python, and Javascript (React and NodeJS).",
+    links: {
+      linkedin: "https://www.linkedin.com/in/luc-somers/",
+      twitter: "https://twitter.com/LucSomers11"
+    },
+    scores: {
+      analytic: "20%",
+      pragmatic: "40%",
+      kind: "20%",
+      expressive: "20%"
+    },
+    topics: ["Canadian", "South Park", "Futurama", "Eu Gostar Disso"]
+  },
+  vanessa: {
+    profilePicture:
+      "https://media.licdn.com/dms/image/C4D03AQGQ7x37Aynx1g/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=Fc1fyDKq999N5yuTLLVCb5vw11YGN54YhgdObbaeMVQ",
+    name: "Vanessa Araújo",
+    email: "vanessa.araujo@itau.com.br",
+    company: "Itaú Unibanco",
+    position: "Analista de Engenharia de TI",
+    city: "São Paulo",
+    country: "Brasil",
+    quote: "Para de fazer Memes meus Eric!",
+    links: {
+      linkedin: "https://www.linkedin.com/in/vanessa-ara%C3%BAjo-371988159/",
+      twitter: "https://twitter.com/AConstancia3"
+    },
+    scores: {
+      analytic: "20%",
+      pragmatic: "25%",
+      kind: "35%",
+      expressive: "20%"
+    },
+    topics: ["Privatizar videos do Youtube", "C#", "Itaú", "Roberto Carlos"]
+  },
+  yury: {
+    profilePicture:
+      "https://media.licdn.com/dms/image/C4D03AQEQ18ooLodV_Q/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=VossjwUYkV_sK5x5Vdr1cCosW-stki0bKP08KwQKAF8",
+    name: "Yury Tinos",
+    email: "yury@expensemobi.com.br",
+    company: "Expense Mobi",
+    position: "Enterprise Architecture",
+    city: "São Paulo",
+    country: "Brasil",
+    quote: "Tô no Tinder...",
+    links: {
+      linkedin: "https://www.linkedin.com/in/yurytinos/",
+      twitter: "https://twitter.com/TinosYury"
+    },
+    scores: {
+      analytic: "50%",
+      pragmatic: "20%",
+      kind: "20%",
+      expressive: "10%"
+    },
+    topics: ["Java", "Within Temptation", "FIAP", "Cerveja Colorado"]
   }
 };
+
 export default people;
